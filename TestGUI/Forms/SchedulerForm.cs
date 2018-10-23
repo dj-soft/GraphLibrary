47a6c40370d71a60699e2096cb4d8fd2994c2b1d
dj-soft <dj-soft@users.noreply.github.com>
2018-10-23 22:04:06 +0200

    Testovací datový zdroj

---
+++ TestGUI/Forms/SchedulerForm.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using GUI = Noris.LCS.Base.WorkScheduler;
+using WS = Asol.Tools.WorkScheduler.Scheduler;
+using Asol.Tools.WorkScheduler.Application;
+
+namespace Asol.Tools.WorkScheduler.TestGUI
+{
+    public partial class SchedulerForm : Form
+    {
+        public SchedulerForm()
+        {
+            InitializeComponent();
+            InitializeScheduler();
+        }
+        protected void InitializeScheduler()
+        {
+            try
+            {
+                App.AppCompanyName = "Asseco Solutions";
+                App.AppProductName = "WorkScheduler";
+                App.AppProductTitle = "Dílenské plánování";
+
+                this.SourceData = new SchedulerData();
+                this.GuiData = this.SourceData.CreateGuiData();
+
+                using (var scope = Application.App.Trace.Scope(Application.TracePriority.Priority3_BellowNormal, "PluginForm", "InitializeWorkScheduler", ""))
+                {
+                    this.MainData = new WS.MainData(this.SourceData as IAppHost);
+                    this.MainData.LoadData(this.GuiData);
+                    this.MainData.CreateControlToForm(this);
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("Při spouštění WorkScheduleru došlo k chybě:" + Environment.NewLine + exc.Message, "Chyba", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            }
+        }
+        protected SchedulerData SourceData;
+        protected GUI.GuiData GuiData;
+        protected WS.MainData MainData;
+    }
+}
