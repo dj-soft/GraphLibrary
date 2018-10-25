b4eee47a89740f997589ec806d98e2eef69550e9
dj-soft <dj-soft@users.noreply.github.com>
2018-10-25 14:19:03 +0200

    Zápisy událsotí do Trace při startu Scheduleru, pro TimeOptimalizaci

--- TestGUI/Forms/SchedulerForm.cs
+++ TestGUI/Forms/SchedulerForm.cs
@@ -18,31 +18,59 @@ namespace Asol.Tools.WorkScheduler.TestGUI
     {
         public SchedulerForm()
         {
-            InitializeComponent();
-            InitializeScheduler();
+            InitializeApplication();
+
+            using (App.Trace.Scope("SchedulerForm", "InitializeComponent", ""))
+                InitializeComponent();
+
+            using (App.Trace.Scope("SchedulerForm", "InitializeScheduler", ""))
+                InitializeScheduler();
+        }
+        protected void InitializeApplication()
+        {
+            App.AppCompanyName = "Asseco Solutions";
+            App.AppProductName = "WorkScheduler";
+            App.AppProductTitle = "Dílenské plánování";
+            App.Trace.Info("SchedulerForm", "InitializeApplication", "", "First row");
         }
         protected void InitializeScheduler()
         {
             try
             {
-                App.AppCompanyName = "Asseco Solutions";
-                App.AppProductName = "WorkScheduler";
-                App.AppProductTitle = "Dílenské plánování";
+                using (App.Trace.Scope("SchedulerDataSource", ".ctor", ""))
+                    this.DataSource = new SchedulerDataSource();
 
-                this.DataSource = new SchedulerDataSource();
-                this.GuiData = this.DataSource.CreateGuiData();
+                using (App.Trace.Scope("SchedulerDataSource", "CreateGuiData", ""))
+                    this.GuiData = this.DataSource.CreateGuiData();
 
-                using (var scope = Application.App.Trace.Scope(Application.TracePriority.Priority3_BellowNormal, "PluginForm", "InitializeWorkScheduler", ""))
+                using (var scope = App.Trace.Scope(Application.TracePriority.Priority3_BellowNormal, "SchedulerForm", "Initialize.MainData", ""))
                 {
-                    this.MainData = new WS.MainData(this.DataSource as IAppHost);
-                    this.MainData.LoadData(this.GuiData);
-                    this.MainData.CreateControlToForm(this);
+                    using (App.Trace.Scope("MainData", ".ctor", ""))
+                        this.MainData = new WS.MainData(this.DataSource as IAppHost);
+
+                    using (App.Trace.Scope("MainData", "LoadData", ""))
+                        this.MainData.LoadData(this.GuiData);
+
+                    using (App.Trace.Scope("MainData", "CreateControlToForm", ""))
+                        this.MainData.CreateControlToForm(this);
                 }
             }
             catch (Exception exc)
             {
                 MessageBox.Show("Při spouštění WorkScheduleru došlo k chybě:" + Environment.NewLine + exc.Message, "Chyba", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
             }
+        }
+        protected override void OnShown(EventArgs e)
+        {
+            using (App.Trace.Scope("SchedulerForm", "OnShown", ""))
+                base.OnShown(e);
+        }
+        protected override void OnClosed(EventArgs e)
+        {
+            using (App.Trace.Scope("SchedulerForm", "OnClosed", ""))
+                base.OnClosed(e);
+
+            Clipboard.SetText(App.Trace.File);
         }
         protected SchedulerDataSource DataSource;
         protected GUI.GuiData GuiData;
