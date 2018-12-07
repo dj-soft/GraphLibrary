76c8b9da02559d28a3173722ce7133cd07ddaf7e
dj-soft <dj-soft@users.noreply.github.com>
2018-12-07 07:39:16 +0100

    Chyba v určení souřadnic GraphItem s časem T=0

--- TestGUI/Program.cs
+++ TestGUI/Program.cs
@@ -20,8 +20,8 @@ namespace Asol.Tools.WorkScheduler.TestGUI
 
             Application.App.TracePriority = Application.TracePriority.Priority5_Normal;
 
-            Application.App.RunMainForm(typeof(SchedulerForm));            // interní testovací data
-            // Application.App.RunMainForm(typeof(PluginForm));               // data z Greenu
+            // Application.App.RunMainForm(typeof(SchedulerForm));            // interní testovací data
+            Application.App.RunMainForm(typeof(PluginForm));               // data z Greenu
 
 
             // Application.App.RunMainForm(typeof(Asol.Tools.WorkScheduler.TestGUI.Forms.TestSnapForm));
