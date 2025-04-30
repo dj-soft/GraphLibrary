f9df8453b2d93d30f5524602770daf4396908ce1
dj-soft <dj-soft@seznam.cz>
2025-04-30 16:00:01 +0200

    as

--- TestDevExpress/Forms/DxDialogTestForm.cs
+++ TestDevExpress/Forms/DxDialogTestForm.cs
@@ -22,19 +22,48 @@ namespace TestDevExpress.Forms
             this.Text = $"DialogBox tester [{Counter}]";
 
             // Přidej buttony, jejich Text je zobrazen uživateli a současně je klíčem do metody _Click:
-            DxComponent.CreateDxSimpleButton(40, 20, 280, 37, this.DxMainPanel, "Dialog [ OK ]", _Click);
-            DxComponent.CreateDxSimpleButton(40, 60, 280, 37, this.DxMainPanel, "Dialog [ OK ] / Center", _Click);
-            DxComponent.CreateDxSimpleButton(40, 100, 280, 37, this.DxMainPanel, "Dialog s detailem", _Click);
-            DxComponent.CreateDxSimpleButton(40, 140, 280, 37, this.DxMainPanel, "XXXX", _Click);
-            DxComponent.CreateDxSimpleButton(40, 180, 280, 37, this.DxMainPanel, "XXXX", _Click);
-            DxComponent.CreateDxSimpleButton(40, 220, 280, 37, this.DxMainPanel, "XXXX", _Click);
+            DxComponent.CreateDxSimpleButton(40, 20, 280, 37, this.DxMainPanel, "Dialog [ OK ]", _Click, tag: "DialogOK");
+            DxComponent.CreateDxSimpleButton(40, 60, 280, 37, this.DxMainPanel, "Dialog [ OK ] / Center", _Click, tag: "DialogOKCenter");
+            DxComponent.CreateDxSimpleButton(40, 100, 280, 37, this.DxMainPanel, "Dialog s detailem", _Click, tag: "DialogDetail");
+            DxComponent.CreateDxSimpleButton(40, 140, 280, 37, this.DxMainPanel, "Ano / Ne", _Click, tag: "AnoNe");
+            DxComponent.CreateDxSimpleButton(40, 180, 280, 37, this.DxMainPanel, "v poho / zapomeň", _Click, tag: "VPohoZapom");
+            // DxComponent.CreateDxSimpleButton(40, 220, 280, 37, this.DxMainPanel, "XXXX", _Click, tag: "DialogOK");
+
+
+            __ResultLabel = DxComponent.CreateDxLabel(40, 500, 300, this.DxMainPanel, "", LabelStyleType.MainTitle);
+
         }
+        protected override void DxMainContentDoLayout(bool isSizeChanged)
+        {
+            if (__ResultLabel != null)
+                __ResultLabel.Bounds = new System.Drawing.Rectangle(40, this.DxMainPanel.ClientSize.Height - 12 - __ResultLabel.Height, 500, __ResultLabel.Height);
+
+        }
+        private string _ResultText
+        {
+            get
+            {
+                if (__ResultLabel != null)
+                    return __ResultLabel.Text;
+                return null;
+            }
+            set
+            {
+                if (__ResultLabel != null)
+                    __ResultLabel.Text = value;
+            }
+        }
+        private DxLabelControl __ResultLabel;
         private static int Counter = 0;
         private void _Click(object sender, EventArgs e) 
         {
+            _ResultText = "";
+
             if (!(sender is DxSimpleButton button)) return;
-            string text = button.Text;
-            if (string.IsNullOrEmpty(text)) return;
+            if (!(button.Tag is string)) return;
+            string command = button.Tag as string;
+            if (string.IsNullOrEmpty(command)) return;
+            string title = button.Text;
 
             DialogArgs dialogArgs = new DialogArgs() { SystemIcon = DialogSystemIcon.None };
 
@@ -42,17 +71,17 @@ namespace TestDevExpress.Forms
             string resourceCopy2 = "images/xaf/templatesv2images/action_copy.svg";
             string resourceAltx1 = "svgimages/diagramicons/showprintpreview.svg";
 
-            switch (text)
+            switch (command)
             {
-                case "Dialog [ OK ]":
-                    dialogArgs.Title = "Dialog [OK]; " + Randomizer.GetSentences(3, 7, 4, 6);
+                case "DialogOK":
+                    dialogArgs.Title = title;
                     dialogArgs.SystemIcon = DialogSystemIcon.Information;
                     dialogArgs.PrepareButtons(DialogResult.OK);
                     dialogArgs.MessageText = Randomizer.GetSentences(4, 8, 3, 12);
                     dialogArgs.StatusBarCtrlCImage = resourceCopy1;
                     break;
-                case "Dialog [ OK ] / Center":
-                    dialogArgs.Title = text;
+                case "DialogOKCenter":
+                    dialogArgs.Title = title;
                     dialogArgs.SystemIcon = DialogSystemIcon.Information;
                     dialogArgs.PrepareButtons(DialogResult.OK);
                     dialogArgs.MessageText = "Jistě, pane ministře.";
@@ -61,8 +90,8 @@ namespace TestDevExpress.Forms
                     dialogArgs.StatusBarCtrlCText = null;
                     dialogArgs.StatusBarVisible = false;
                     break;
-                case "Dialog s detailem":
-                    dialogArgs.Title = text;
+                case "DialogDetail":
+                    dialogArgs.Title = title;
                     dialogArgs.SystemIcon = DialogSystemIcon.Information;
                     dialogArgs.PrepareButtons(DialogResult.OK);
                     dialogArgs.MessageText = Randomizer.GetSentences(4, 8, 3, 12);
@@ -71,7 +100,29 @@ namespace TestDevExpress.Forms
                     dialogArgs.StatusBarCtrlCImage = resourceCopy1;
                     dialogArgs.StatusBarAltMsgButtonImage = resourceAltx1;
                     break;
+                case "AnoNe":
+                    dialogArgs.Title = title;
+                    dialogArgs.SystemIcon = DialogSystemIcon.Question;
+                    dialogArgs.PrepareButtons(DialogResult.Yes, DialogResult.No);
+                    dialogArgs.MessageText = Randomizer.GetSentences(4, 8, 3, 12);
+                    dialogArgs.AltMessageText = Randomizer.GetSentences(4, 12, 12, 24);
+                    dialogArgs.ButtonsAlignment = AlignContentToSide.Center;
+                    dialogArgs.StatusBarCtrlCImage = resourceCopy1;
+                    dialogArgs.StatusBarAltMsgButtonImage = resourceAltx1;
+                    break;
+                case "VPohoZapom":
+                    dialogArgs.Title = title;
+                    dialogArgs.SystemIcon = DialogSystemIcon.Question;
+                    dialogArgs.Buttons.Add(new DialogArgs.ButtonInfo() { Text = "v pohodě", IsImplicitButton = true, IsInitialButton = true, ActiveKey = Keys.A, ResultValue = 1, StatusBarText = "Tohle je v pohodě" });
+                    dialogArgs.Buttons.Add(new DialogArgs.ButtonInfo() { Text = "na to zapomeň", IsImplicitButton = false, IsInitialButton = false, ActiveKey = Keys.N, ResultValue = 2, StatusBarText = "Tudy cesta nevede" });
+                    dialogArgs.MessageText = Randomizer.GetSentences(4, 8, 3, 12);
+                    dialogArgs.ButtonsAlignment = AlignContentToSide.Center;
+                    dialogArgs.StatusBarCtrlCImage = resourceCopy1;
+                    dialogArgs.StatusBarAltMsgButtonImage = resourceAltx1;
+                    break;
+
                 default:
+                    /*
                     var printers = PdfPrinter.InstalledPrinters;
                     var defPrinter = PdfPrinter.DefaultPrinterName;
 
@@ -83,7 +134,7 @@ namespace TestDevExpress.Forms
                     string fileName = _GetRandomPdf();
                     // PdfPrinter.PrintWithProcess(fileName, pa);
                     PdfPrinter.PrintWithControl(fileName, pa);
-
+                    */
                     break;
             }
 
@@ -93,6 +144,7 @@ namespace TestDevExpress.Forms
                 dialogArgs.Owner = this;
                 var result = DialogForm.ShowDialog(dialogArgs);
 
+                _ResultText = (result is null ? "" : "ResultDialog: " +  result.ToString());
             }
         }
         private static string _GetRandomPdf()
